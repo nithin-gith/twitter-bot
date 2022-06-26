62cfb6721a77b424e973684db03d6828f29abe09
nithin-gith <nithin.siriguppa@gmail.com>
2022-06-26 12:03:53 +0530

    intial commit

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,18 +1,21 @@
+from json import load
 import tweepy
 import time
 
-from os import environ as env
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv("CONSUMER_KEY"))
 
 
-consumer_key=env["CONSUMER_KEY"]
-consumer_secret=env["CONSUMER_SECRET_KEY"]
-access_token=env["TWITTER_ACCESS_TOKEN"]
-access_token_secret=env["TWITTER_ACCESS_TOKEN_SECRET"]
+consumer_key=os.getenv("CONSUMER_KEY")
+consumer_secret=os.getenv("CONSUMER_SECRET_KEY")
+access_token=os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 
-consumer_key,consumer_secret,access_token,access_token_secret
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret,access_token,access_token_secret)
                                  
 api = tweepy.API(auth)
 
@@ -22,26 +25,49 @@ try:
 except:
     print("not so cool")
 
-my_id = api.get_user(screen_name="bot_jff").id
+my_id = api.get_user(screen_name="SiriguppaNithin").id
 mention_id=1
 reply = "Please check your DM"
 message = "Hello, How may I help You :)"
 
+# api.send_direct_message(my_id, "Hi Bro from python")
 
-while 1 :
-    mentions=api.mentions_timeline(since_id=mention_id)
+mentions = api.mentions_timeline(count=1)
+while True:
     for mention in mentions:
-        if mention.created_at.timestamp()<time.time():
-            continue
-        print("Mention tweet found!")
-        mention_id = mention.id
-        if mention.author.id != my_id and mention.in_reply_to_status_id is None:
-            api.send_direct_message(mention.author.id,message)
-            try:
-                api.update_status(reply,in_reply_to_mention_status_id=mention.id_str)
-                print("replying")
-            except:
-                print("failed")
+        print(mention.text)
+    time.sleep(5)
+
+# if api.destroy_friendship(id=api.get_user(screen_name="Sreya_TV").id):
+#     print ("Followed")
+# else:
+#     print ("Not Followed")
+
+# tweets = api.user_timeline(id=my_id)
+# for tweet in tweets:
+#     print(str(tweet.id)+'-'+tweet.text)
+
+
+
+
+
+# while 1 :
+#     mentions=api.mentions_timeline(since_id=mention_id)
+#     for mention in mentions:
+#         if mention.created_at.timestamp()<time.time():
+#             continue
+#         print("Mention tweet found!")
+#         mention_id = mention.id
+#         if mention.author.id != my_id and mention.in_reply_to_status_id is None:
+#             api.send_direct_message(mention.author.id,message)
+#             try:
+#                 api.update_status(reply,in_reply_to_mention_status_id=mention.id_str)
+#                 print("replying")
+#             except:
+#                 print("failed")
+
+
+
 # while 1:
 #     mentions = api.mentions_timeline()
 #     for mention in mentions:
